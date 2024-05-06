0e76ab18521be0cc7200785d05e7270d6d4f777c
Saff-Buraq-Dev <gharbi.safwen@hotmail.com>
2024-05-06 19:37:18 -0400

    fix base path

--- next.config.js
+++ next.config.js
@@ -2,8 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  basePath: '/saff-portfolio',
-  assetPrefix: '/saff-portfolio/',
-}
+  i18n: {
+    locales: ['en'],
+    defaultLocale: 'en',
+  }
+};
 
 module.exports = nextConfig
