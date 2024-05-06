3f4ec2c701202e7184d9f05969b717fd5b49b6bf
Saff-Buraq-Dev <gharbi.safwen@hotmail.com>
2024-05-06 19:07:45 -0400

    make it static website

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  basePath: '/saff-portfolio',
+  assetPrefix: '/saff-portfolio/',
   i18n: {
     locales: ["en"],
     defaultLocale: "en",
