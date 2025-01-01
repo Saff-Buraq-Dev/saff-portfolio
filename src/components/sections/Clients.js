b1c120224dcc21ac796a2b82d276930cac531992
Saff-Buraq-Dev <gharbi.safwen@hotmail.com>
2025-01-01 13:31:44 -0500

    maj cv

--- src/components/sections/Clients.js
+++ src/components/sections/Clients.js
@@ -24,6 +24,7 @@ const Clients = () => {
             </div>
           </div>
 
+          {/* client item */}
           <div className="col col-d-3 col-t-3 col-m-6 border-line-v">
             <div className="client-item">
               <div className="image">
@@ -32,7 +33,7 @@ const Clients = () => {
                   rel="noreferrer"
                   href="https://www.credly.com/badges/2e551850-8844-41ed-81d4-b30dfa0325f6/public_url"
                 >
-                  <img src="images/aws-certified-developer-associate.png" alt="Terraform certified associate" />
+                  <img src="images/aws-certified-developer-associate.png" alt="AWS certified Developer associate" />
                 </a>
               </div>
             </div>
@@ -48,6 +49,36 @@ const Clients = () => {
                   href="https://www.credly.com/badges/d8324061-f5e2-4c77-83c2-0fe230907a35/public_url"
                 >
                   <img src="images/terraform.webp" alt="Terraform certified associate" />
+                </a>
+              </div>
+            </div>
+          </div>
+
+          {/* client item */}
+          <div className="col col-d-3 col-t-3 col-m-6 border-line-v">
+            <div className="client-item">
+              <div className="image">
+                <a
+                  target="_blank"
+                  rel="noreferrer"
+                  href="https://www.credly.com/badges/ffd934b2-d711-47c5-bdd1-b51ef237cee8/public_url"
+                >
+                  <img src="images/aws-certified-ai-practitioner.png" alt="AWS certified AI practitioner" />
+                </a>
+              </div>
+            </div>
+          </div>
+
+          {/* client item */}
+          <div className="col col-d-3 col-t-3 col-m-6 border-line-v">
+            <div className="client-item">
+              <div className="image">
+                <a
+                  target="_blank"
+                  rel="noreferrer"
+                  href="https://www.credly.com/badges/fbda65fc-e041-44fe-acb2-53a144a6a4fe/public_url"
+                >
+                  <img src="images/aws-certified-cloud-practitioner.png" alt="AWS certified Cloud practitioner" />
                 </a>
               </div>
             </div>
